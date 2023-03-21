4031c7194587e05bab1a4b6a7a4f3e244866e59e
SriramKarthikKumar <sreejiit1989@gmail.com>
2023-03-21 12:03:18 +0800

    Updating specific queries
    Adding Weekly, Monthly, Quarterly and Yearly average computations

--- msft/SQL_Queries.sql
+++ msft/SQL_Queries.sql
@@ -50,4 +50,64 @@ CREATE TABLE calculations
 
 ----------------------------------------------------------------------------------------------
 
--- Queries to find our weekly, monthly, quarterly, yearly averages of
+-- Queries to find our weekly, monthly, quarterly, yearly averages of High, Low, Volume
+
+----------------------------------------------------------------------------------------------
+-- Yearly Average Calculations
+SELECT avg(high), year_val
+	FROM public.trading
+	group by year_val
+----------------------------------------------------------
+-- Result
+-- High Average          Year
+--73.71009195852538	    2017
+--131.23095248015883	2019
+--278.0180160198413	    2021
+--195.4651384229249	    2020
+--310.66911585294116	2022
+--102.11406372509963	2018
+----------------------------------------------------------
+-- Monthly Average calculations
+SELECT avg(high) as "Avg High", avg(low) as "Avg Low", avg(volume) as "Avg Volume",
+year_val, month_val
+FROM public.trading
+group by month_val, year_val
+
+------------------------------------------------------------------------------
+--  Result
+-- High Average          Low Average         Volume Average     Year   Month
+--157.54500095454546	147.59863563636364	73304340.9090909	2020	3
+--85.40950040000001	    84.16350020000002	23310165	        2017	12
+--133.10750045000003	130.85050059999998	25416215	        2019	6
+--92.76578884210524	    90.05157957894737	38192805.2631579	2018	2
+--241.96736636842107	237.45157668421052	25840115.789473683	2021	2
+--101.33285680952382	99.8576188095238	28694533.333333332	2018	6
+--148.55350254999996	147.15550084999995	19618590	        2019	11
+--104.97809552380951	102.85095204761903	34010133.333333336	2019	1
+------------------------------------------------------------------------------
+
+-- Weekly Average calculations for year 2019 and month March
+SELECT avg(high) as "Avg High", avg(low) as "Avg Low", avg(volume) as "Avg Volume",
+week_val
+FROM public.trading
+where year_val = '2019' and month_val = '3'
+group by week_val
+
+------------------------------------------------------------------------------
+--  Result
+-- High Average          Low Average         Volume Average     week
+-- 116.31428528571429	114.20285671428573	24037714.285714287	11
+-- 115.70071414285714	114.0264287857143	30059414.285714287	12
+
+------------------------------------------------------------------------------
+
+-- Quarterly Average calculations for year 2019 and Q2 March
+SELECT avg(high) as "Avg High", avg(low) as "Avg Low", avg(volume) as "Avg Volume"
+, '2019 - Q1' as Quarter
+FROM public.trading
+where year_val = '2019' and month_val in (1,2,3)
+
+------------------------------------------------------------------------------
+--  Result
+-- High Average          Low Average         Volume Average      Quarter
+-- 109.82163934426225	107.98032786885244	29055811.475409836	"2019 - Q1"
